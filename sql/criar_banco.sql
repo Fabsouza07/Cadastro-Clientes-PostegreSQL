828b918c9714b3a87f09fae7b6db146c132e32fb
Fabsouza07 <fabsouza1@hotmail.com>
2026-09-12 16:51:29 -0300

    Corrige a sintaxe do comando CREATE DATABASE no script criar_banco.sql

--- sql/criar_banco.sql
+++ sql/criar_banco.sql
@@ -1,5 +1,6 @@
 -- Execute este arquivo conectado ao banco administrativo "postgres".
 -- O comando CREATE DATABASE não pode ser executado dentro de uma transação.
 CREATE DATABASE cadastro_clientes
-    WITH ENCODING = 'UTF8'
-    TEMPLATE = template0;
+    OWNER postgres
+    ENCODING 'UTF8'
+    TEMPLATE template0;
